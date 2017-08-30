30104d03f8f79fd8dec3b89e34507cef4bc1d668
Fabricio Borgatto <fabricioborgatto@gmail.com>
2017-08-30 04:45:23 -0300

    new file:   public/controller.js

---
+++ public/controller.js
@@ -0,0 +1,64 @@
+
+var pdvManager = angular.module('pdvManager', []);
+
+function mainController($scope, $http) {
+
+    $scope.showPdvAlert = false;
+
+    $scope.findById = function (pdvId) {
+        if (isDefined(pdvId)) {
+            $http.get('/pdv/' + pdvId)
+                .success(function (data) {
+                    $scope.pdv = data;
+                    console.log('Search finished: ' + data);
+                })
+                .error(function (data) {
+                    console.log('Error: ' + data);
+                });
+        }
+    };
+
+    $scope.findByAddressAndCoverage = function () {
+        if (isDefined($scope.queryParameters)) {
+            $http.get('/pdv?', {
+                params: {
+                    latitude: $scope.queryParameters.latitude,
+                    longitude: $scope.queryParameters.longitude
+                }
+            })
+                .success(function (data) {
+                    $scope.pdv = data;
+                    console.log('Search finished: ' + data);
+                })
+                .error(function (data) {
+                    console.log('Error: ' + data);
+                });
+        }
+    };
+
+    $scope.addPdv = function () {
+        $http.post('/pdv', $scope.inputPdvJSON)
+            .success(function (data) {
+                //$scope.pdv = data;
+                if(!data.includes('Error inserting PDV')) {
+                    $scope.showPdvSuccess = true;
+                    $scope.showPdvAlert = false;
+                    console.log('New Pdv added: ' + data);
+                } else {
+                    $scope.showPdvAlert = true;
+                    $scope.showPdvSuccess = false;
+                    console.log(data);
+                }
+            })
+            .error(function (data) {
+                $scope.showPdvAlert = true;
+                $scope.showPdvSuccess = false;
+                console.log('Error: ' + data);
+            });
+    };
+
+    function isDefined(parameter) {
+        return undefined != parameter;
+    }
+
+}
